fdce3d537bae0858bce3efc201879a25b7aaa819
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2023-05-21 10:23:14 +0900

    830

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ t(L);
 pl1 = 1;
 pl2 = 2;
 
-change = z => {
+s = z => {
     a = pl1;
     pl1 = pl2;
     pl2 = a
@@ -20,73 +20,60 @@ check = (x, y) => {
     p = 0;
     mL = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     if (!L[y][x]) {
-        for (py = -1; py < 2; py++) {
-            for (px = -1; px < 2; px++) {
-                ax = px;
-                ay = py;
-                i = 0;
-                while (L[y + ay] && L[y + ay][x + ax] == pl2) {
-                    ax += px;
-                    ay += py;
-                    i++
-                }
-                if (i && L[y + ay][x + ax] == pl1) {
-                    mL[py + 1][px + 1] = 1;
-                    p = 1
-                }
+        for (j = 9; j--;) {
+            px = j % 3 - 1;
+            py = (j / 3 | 0) - 1;
+            ax = x + px;
+            ay = y + py;
+            i = 0;
+            while (L[ay] && L[ay][ax] == pl2) {
+                ax += px;
+                ay += py;
+                i = 1
+            }
+            if (i && L[ay] && L[ay][ax] == pl1) {
+                mL[py + 1][px + 1] = 1;
+                p = 1
             }
         }
     }
     return { p, mL }
 }
 
-checkAll = z => {
-    for (a = 64; a--;) if (check(a % 8, a / 8 | 0).p) return 1;
-    return 0
-}
-
 put = (x, y) => {
     if (!L[y][x]) {
         flg = 0;
         p = check(x, y);
-        for (py = -1; py < 2; py++) {
-            for (px = -1; px < 2; px++) {
+        // 置けることが確認できたとき
+        if (p.p) {
+            for (j = 9; j--;) {
+                px = j % 3 - 1;
+                py = (j / 3 | 0) - 1;
                 if (p.mL[py + 1][px + 1]) {
-                    i = 1;
-                    while (L[y + py * i][x + px * i] == pl2) {
-                        L[y + py * i][x + px * i] = pl1;
-                        i++
-                    }
                     flg = 1
+                    L[y][x] = pl1;
+                    while (L[ay = y + py * flg][ax = x + px * flg] == pl2) L[ay][ax] = pl1, flg++
                 }
             }
-        }
-        q = 1;
-        d = [0, 0, 0];
-        for (i = 64; i--;) {
-            l = L[i / 8 | 0][i % 8];
-            d[l]++;
-            q *= l;
-        }
-        e = 1;
-        for (i of d) e *= i;
-        if (e) {
-            if (flg) {
-                L[y][x] = pl1;
-                change();
-                if (!checkAll()) {
-                    t("パス");
-                    change()
-                }
+            d = [0, 0, 0];
+            // 0,1,2を数える
+            for (i = 64; i--;) d[L[i / 8 | 0][i % 8]]++;
+            t(d);
+            // 0,1,2のいずれも0でないとき
+            if (d[0] * d[1] * d[2]) {
+                s();
+                p = 0;
+                for (a = 64; a--;) p += check(a % 8, a / 8 | 0).p;
+                if (!p) t("パス"), s();
+                t(`${pl1}のターン`)
             }
-            t(L);
-            q ? t("終了") : t(`${pl1}のターン`)
+            else {
+                if (d[1] == d[2]) t("ドロー");
+                else t(`${d.indexOf(Math.max(d[1], d[2]))}の勝ち`)
+            }
         }
-        else {
-            if (d[1] < d[2]) t(`${2}の勝ち`);
-            else if (d[1] > d[2]) t(`${1}の勝ち`);
-            else t("ドロー")
-        }
+        else t("置けません")
     }
-    else t("置けません")
+    else t("置けません");
+    t(L.join("\n"))
 }
